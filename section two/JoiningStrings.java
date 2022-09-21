2c5b464128168698a6439ac125faa963c77d7dcf
LunaDGD <lunadgd@protonmail.com>
2022-09-20 21:51:14 -0400

    first commit

---
+++ section two/JoiningStrings.java
@@ -0,0 +1,11 @@
+public class JoiningStrings {
+    public static void main(String[] args) {
+        String sentence = "His name is: ";
+        String placeholder ="Harry";
+        System.out.println(sentence + placeholder);
+        int points = 50;
+        String announcement = " points for gryffindor";
+        System.out.println(points+announcement);
+    }
+    
+}
---
+++ section two/WholeNumbers.java
@@ -0,0 +1,11 @@
+public class WholeNumbers {
+    public static void main(String[] args) {
+        int numberOfFans = 5000;
+        System.out.println("There are " + numberOfFans + " fans at the games");
+        // add the L at the end of a number
+        long globalPopulation = 7800000000L;
+        System.out.println("There are " + globalPopulation + " people on earth");
+
+    }
+    
+}
